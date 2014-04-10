073fc0096fe299aeeec0e81f746c615ba651b596
Quyse Lert <quyse0@gmail.com>
2014-04-10 21:34:19 +0000

    support for install upgrades; version now stored in text file

--- client/installer/README.md
+++ client/installer/README.md
@@ -1,10 +1,12 @@
 # Packaging Oil Client
 
-## Bootstrap tools
+## Bootstrap
 
+* Create directories `tools` and `out`
 * Download latest xulrunner into `tools/xulrunner` from [http://ftp.mozilla.org/pub/mozilla.org/xulrunner/releases/latest/runtimes]
 * Copy `setenv.bat.tmpl` as `tools/setenv.bat`, and fill with right paths to WiX and 7zip.
 
-## Building
+## Making new version
 
-* run `build.bat`
+* put right version into `version.txt`
+* run `build.bat`. WARNING: result .msi will be created in `C:\tmp\`! That's because of strange glitches when it's generating in `out` directory.
--- client/installer/build.bat
+++ client/installer/build.bat
@@ -1,10 +1,18 @@
 @echo off
 call tools\setenv.bat
 
+rem get version
+set /p version=<version.txt
+
 echo ======================= make content.jar
 
 erase /Q out\*
 7z a -tzip -mx=9 -mcu=on out\content.jar ..\chrome\content
+
+
+echo ======================= make application.ini
+
+call :appini > out\application.ini
 
 
 echo ======================= make windows installer package
@@ -12,9 +20,9 @@ echo ======================= make windows installer package
 call :heatdir ..\..\assets assets APPLICATIONFOLDER
 call :heatdir tools\xulrunner xulrunner APPLICATIONFOLDER
 
-candle -nologo oilclient.wxs -out out\oilclient.wixobj
+candle -nologo oilclient.wxs -out out\oilclient.wixobj -dversion=%version%
 
-light -nologo -ext WixUIExtension -cultures:en-us -out C:\tmp\oilclient.msi out\oilclient.wixobj out\assets.wixobj out\xulrunner.wixobj
+light -nologo -ext WixUIExtension -cultures:en-us -out C:\tmp\oilclient_%version%.msi out\oilclient.wixobj out\assets.wixobj out\xulrunner.wixobj
 
 goto :eof
 
@@ -24,4 +32,15 @@ rem === functions
 :heatdir
 heat dir %1 -nologo -sfrag -scom -sreg -gg -g1 -cg %2 -dr %3 -var var.%2_source -out out\%2.wxs
 candle -nologo out\%2.wxs -out out\%2.wixobj -d%2_source=%1
+goto :eof
+
+:appini
+echo Vendor=quyse.net
+echo Name=oil
+echo Version=%version%
+echo BuildID=1234567
+echo ID=oil@quyse.net
+echo [Gecko]
+echo MinVersion=1.8
+echo MaxVersion=200.*
 goto :eof
