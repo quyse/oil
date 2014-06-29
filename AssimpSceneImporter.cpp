8168f0bf2353d2a91bdc139263436edcb7dcffec
Quyse Lert <quyse0@gmail.com>
2014-06-29 17:51:49 +0400

    AssimpSceneImporter work-in-progress

--- AssimpSceneImporter.cpp
+++ AssimpSceneImporter.cpp
@@ -1,10 +1,17 @@
 #include "AssimpSceneImporter.hpp"
 #include "Model.hpp"
 #include "ImportedScene.hpp"
-#include "../inanity/File.hpp"
+#include "Material.hpp"
+#include "../inanity/graphics/RawMesh.hpp"
+#include "../inanity/graphics/VertexLayout.hpp"
+#include "../inanity/MemoryFile.hpp"
+#include "../inanity/Exception.hpp"
 #include "../inanity/deps/assimp/repo/include/assimp/Importer.hpp"
 #include "../inanity/deps/assimp/repo/include/assimp/scene.h"
 #include "../inanity/deps/assimp/repo/include/assimp/mesh.h"
+#include "../inanity/deps/assimp/repo/include/assimp/postprocess.h"
+
+using namespace Inanity::Graphics;
 
 BEGIN_INANITY_OIL
 
@@ -46,14 +53,74 @@ ptr<ImportedScene> AssimpSceneImporter::Import(ptr<File> file)
 	models.resize(aScene->mNumMeshes);
 	for(size_t i = 0; i < models.size(); ++i)
 	{
-		aiMesh* aMesh = aScene->mMeshes[i];
+		const aiMesh* aMesh = aScene->mMeshes[i];
+
+		// convert vertices
 
 		size_t verticesCount = aMesh->mNumVertices;
-		aiVector3D* positions = aMesh->mVertices;
-		aiVector3D* normals = aMesh->mNormals;
-		aiVector3D* tangents = aMesh->mTangents;
-		aiVector3D* bitangents = aMesh->mBitangents;
-		aiVector3D* textureCoords = aMesh->mTextureCoords[0];
+		const aiVector3D* positions = aMesh->mVertices;
+		const aiVector3D* tangents = aMesh->mTangents;
+		const aiVector3D* bitangents = aMesh->mBitangents;
+		const aiVector3D* normals = aMesh->mNormals;
+		const aiVector3D* textureCoords = aMesh->mTextureCoords[0];
+
+		struct Vertex
+		{
+			vec3 position;
+			vec3 tangent;
+			vec3 binormal;
+			vec3 normal;
+			vec2 texcoord;
+		};
+
+		auto c3 = [](const aiVector3D& v) { return vec3(v.x, v.y, v.z); };
+		auto c2 = [](const aiVector3D& v) { return vec2(v.x, v.y); };
+
+		ptr<MemoryFile> verticesFile = NEW(MemoryFile(verticesCount * sizeof(Vertex)));
+		Vertex* vertices = (Vertex*)verticesFile->GetData();
+		for(size_t i = 0; i < verticesCount; ++i)
+		{
+			Vertex& vertex = vertices[i];
+			vertex.position = c3(positions[i]);
+			vertex.tangent = c3(tangents[i]);
+			vertex.binormal = c3(bitangents[i]);
+			vertex.normal = c3(normals[i]);
+			vertex.texcoord = c2(textureCoords[i]);
+		}
+
+		// convert indices
+
+		size_t facesCount = aMesh->mNumFaces;
+		const aiFace* faces = aMesh->mFaces;
+
+		size_t indicesCount = facesCount * 3;
+		ptr<MemoryFile> indicesFile;
+		// if we have to use big indices
+		if(indicesCount > 0x10000)
+		{
+			indicesFile = NEW(MemoryFile(facesCount * 3 * sizeof(unsigned int)));
+			unsigned int* indices = (unsigned int*)indicesFile->GetData();
+			for(size_t i = 0; i < facesCount; ++i)
+			{
+				const aiFace& face = faces[i];
+				for(size_t j = 0; j < 3; ++j)
+					indices[i * 3 + j] = face.mIndices[j];
+			}
+		}
+		// else we can use small indices
+		else
+		{
+			indicesFile = NEW(MemoryFile(facesCount * 3 * sizeof(unsigned short)));
+			unsigned short* indices = (unsigned short*)indicesFile->GetData();
+			for(size_t i = 0; i < facesCount; ++i)
+			{
+				const aiFace& face = faces[i];
+				for(size_t j = 0; j < 3; ++j)
+					indices[i * 3 + j] = (unsigned short)face.mIndices[j];
+			}
+		}
+
+		models.push_back(NEW(Model(NEW(RawMesh(verticesFile, nullptr, indicesFile)), nullptr)));
 	}
 
 	return scene;
